809bfb15b83bf62a1c9c729f2683e18918c3a933
Brenna Lepage <lepage8563@saskpolytech.ca>
2024-12-18 22:16:37 -0600

    Image Problem Solved -Images displaying -Moved to app.js -Not clickable yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './zelda-logo.png';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
@@ -7,9 +8,28 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-    {}
-
+      <div id="items">
+    <div id="link">
+      <img src="media/link.png"></img>
     </div>
+    <div id="silent-princess">
+      <img src="media/silent-princess.png"/>
+    </div>
+    <div id="bokoblin">
+      <img src="media/bokoblin.png"/>
+    </div>
+    <div id="sacred-ground">
+      <img src="media/sacred-ground-ruins.png"/>
+    </div>
+    <div id="honeyed-apple">
+      <img src="media/honeyed-apple.png"/>
+    </div>
+    <div id="master-sword">
+      <img src="media/master sword.png"/>
+    </div>
+  </div> 
+    </div>
+    
   );
 }
 
