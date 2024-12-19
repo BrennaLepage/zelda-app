2cdb94f2b4ce2fb5e8817eb5bcf9a370dfcfc96e
Brenna Lepage <lepage8563@saskpolytech.ca>
2024-12-18 20:40:06 -0600

    Trying to render images in app instead -CSS wasn't working with images in index -Flex would make images cover the header somehow -Trying to render images in the app instead

--- src/App.js
+++ src/App.js
@@ -7,6 +7,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div id="images">
+    <div id="root"></div>
+    <div id="link">
+      <img src="media/link.png"></img>
+    </div>
+    <div id="silent-princess">
+      <img src="media/silent-princess.png"></img>
+    </div>
+    <div id="bokoblin">
+      <img src="media/bokoblin.png"></img>
+    </div>
+    <div id="sacred-ground">
+      <img src="media/sacred-ground-ruins.png"></img>
+    </div>
+    <div id="honeyed-apple">
+      <img src="media/honeyed-apple.png"></img>
+    </div>
+    <div id="master-sword">
+      <img src="media/master sword.png"></img>
+    </div>
+  </div>
     </div>
   );
 }
