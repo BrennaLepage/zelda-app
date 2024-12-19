9ec92837c92d87cd731b88b4777ab9ef7f23088d
Brenna Lepage <lepage8563@saskpolytech.ca>
2024-12-19 15:47:00 -0600

    Items Clicked -Console.log appearing when items are clicked -Still static -Can't figure out how to loop through to find clicked item

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
+import React, { useState, useEffect } from 'react';
 import logo from './zelda-logo.png';
 import './App.css';
 import './index.css';
 
-const initialItems = [{ id: "link", image: "media/link.png" }, { id: "silent-princess", image: "media/silent-princess.png" },
-{ id: "bokoblin", image: "media/bokoblin.png" }, { id: "sacred-ground", image: "media/sacred-ground-ruins.png" },
-{ id: "honeyed-apple", image: "media/honeyed-apple.png" }, { id: "master-sword", image: "media/master-sword.png" }];
+const initialItems = [{ id: "link", image: "media/link.png", isVisible: true}, { id: "silent-princess", image: "media/silent-princess.png", isVisible: false},
+{ id: "bokoblin", image: "media/bokoblin.png", isVisible: false }, { id: "sacred-ground", image: "media/sacred-ground-ruins.png", isVisible: false },
+{ id: "honeyed-apple", image: "media/honeyed-apple.png", isVisible: false }, { id: "master-sword", image: "media/master-sword.png", isVisible: false }];
 
-
-function ItemList(props) {
-  return (
-    <div>
-      {props.items.map((item) => (
-        <Item id={item.id} image={item.image} />
-      ))}
-    </div>
-  )
-}
-function Item(props) {
-
-  return (
-    <div id={props.id}>
-      <img src={props.image}></img>
-      <p class="description">This is the description.</p>
-    </div>
-  )
-}
 
 
 function App() {
+  
+  // useState - something that will change - function to change it
+  const [items, setItems] = useState(initialItems);
+  
+  const itemClicked = (id) => {
+    console.log("hello");
+    console.log(id)
+    //Create new array with all of the old items
+    const newItems = [{ id: "link"}, { id: "silent-princess"},
+    { id: "bokoblin"}, { id: "sacred-ground"},
+    { id: "honeyed-apple"}, { id: "master-sword"}];
+    //Look through array for item that was clicked and change it
+    //Call setItems with the new array of new items
+    // setItems([])
+  }
+  function ItemList(props) {
+    return (
+      <div>
+        {props.items.map((item) => (
+          <Item id={item.id} image={item.image} isVisible={item.isVisible} />
+        ))}
+      </div>
+    )
+  }
+  
+  function Item(props) {
+  console.log(props);
+    return (
+      <div id={props.id} onClick={() => itemClicked(props.id)}>
+        <img src={props.image}></img>
+        <p style={{display:props.isVisible ? 'block' : 'none'}}>This is the description.</p>
+      </div>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <ItemList items={initialItems} />
+      <ItemList items={items} />
     </div>
 
   );
